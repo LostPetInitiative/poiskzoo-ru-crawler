a417823ce1192b03e671e414832fc06d294faa33
Dmitry Grechka <dmitry@grechka.family>
2022-10-17 13:06:55 +0600

    species extraction & tests

--- pkg/crawler/parser.go
+++ pkg/crawler/parser.go
@@ -2,24 +2,27 @@ package crawler
 
 import (
 	"fmt"
-	"log"
 	"strings"
 
+	"github.com/LostPetInitiative/poiskzoo-ru-crawler/pkg/types"
 	"github.com/antchfx/htmlquery"
+	"golang.org/x/net/html"
 )
 
-func ExtractCardUrlsFromCatalogPage(htmlContent string) []string {
+func ParseHtmlContent(htmlContent string) *html.Node {
 	doc, err := htmlquery.Parse(strings.NewReader(htmlContent))
 	if err != nil {
 		panic(fmt.Sprintf("Failed to parse HTML file: %v", err))
 	}
 
+	return doc
+}
+
+func ExtractCardUrlsFromDocument(doc *html.Node) []string {
 	nodes, err := htmlquery.QueryAll(doc, "//div[contains(@class, 'pzplitkadiv')]//div[contains(@class, 'pzplitkalink')]/a")
 	if err != nil {
 		panic(`not a valid XPath expression.`)
 	}
-
-	log.Printf("nodes count %d\n", len(nodes))
 
 	res := make([]string, len(nodes))
 
@@ -33,4 +36,18 @@ func ExtractCardUrlsFromCatalogPage(htmlContent string) []string {
 	}
 
 	return res
+}
+
+func ExtractSpeciesFromCardPage(doc *html.Node) types.Species {
+	node := htmlquery.FindOne(doc, "//h1[contains(@class, 'con_heading')]")
+	dataText := strings.ToLower(node.FirstChild.Data)
+	switch {
+	case strings.Contains(dataText, "собака"), strings.Contains(dataText, "пес"):
+		return types.Dog
+	case strings.Contains(dataText, "кот"), strings.Contains(dataText, "кошка"):
+		return types.Cat
+	default:
+		panic(fmt.Sprintf("Can't extract species type"))
+	}
+
 }
--- pkg/crawler/parser_test.go
+++ pkg/crawler/parser_test.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"os"
 	"testing"
+
+	"github.com/LostPetInitiative/poiskzoo-ru-crawler/pkg/types"
 )
 
 func TestExtractCardUrlsFromCatalogPage(t *testing.T) {
@@ -13,7 +15,7 @@ func TestExtractCardUrlsFromCatalogPage(t *testing.T) {
 	}
 	catalogHtml := string(fileContent)
 
-	extractedUrls := ExtractCardUrlsFromCatalogPage(catalogHtml)
+	extractedUrls := ExtractCardUrlsFromDocument(ParseHtmlContent(catalogHtml))
 	const expectedCount int = 52
 	if len(extractedUrls) != expectedCount {
 		t.Logf("Expected to extract %d card IDs but extracted %d", expectedCount, len(extractedUrls))
@@ -29,5 +31,31 @@ func TestExtractCardUrlsFromCatalogPage(t *testing.T) {
 	if !exists {
 		t.Logf("Did not find expected URL in the result set")
 		t.Fail()
+	}
+}
+
+func TestExtractSpeciesFromPetCard(t *testing.T) {
+	testCases := []struct {
+		path     string
+		expected types.Species
+	}{
+		{"./testdata/164931.html.dump", types.Dog},
+		{"./testdata/164921.html.dump", types.Cat},
+		{"./testdata/164923.html.dump", types.Cat},
+		{"./testdata/164929.html.dump", types.Dog},
+	}
+
+	for _, testCase := range testCases {
+		fileContent, err := os.ReadFile(testCase.path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		catalogHtml := string(fileContent)
+
+		var extractedSpecies types.Species = ExtractSpeciesFromCardPage(ParseHtmlContent(catalogHtml))
+		if extractedSpecies != testCase.expected {
+			t.Logf("Wrong species extracted for %s. Expected %v, but got %v", testCase.path, testCase.expected, extractedSpecies)
+			t.Fail()
+		}
 	}
 }
